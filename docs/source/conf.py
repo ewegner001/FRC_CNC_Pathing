b2c8ea9805ef467c1efccd1f08d90c2b4971ade9
ewegner001 <88629283+ewegner001@users.noreply.github.com>
2022-06-26 16:59:34 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FRC CNC Pathing'
+copyright = '2022, Everen Wegner'
+author = 'Everen Wegner'
 
 release = '0.1'
 version = '0.1.0'
